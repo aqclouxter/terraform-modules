06978efa45424de640687c2f81a011dcbaf591e4
Alejandro Quintero <alejandro.quintero@clouxter.com>
2025-12-07 11:47:24 -0500

    first commit

--- s3/main.tf
+++ s3/main.tf
@@ -12,10 +12,13 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
-}
 
 variable "bucket_name" {
   type = string
 }
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+}
+
+
